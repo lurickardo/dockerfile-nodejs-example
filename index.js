1fb4b640c53eb2e63a1986ff15a09973ca5dcdc1
Lurickardo <luizr726@gmail.com>
2024-05-10 22:30:58 -0300

    fix: finish dockerfile to run a simple API Node.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const fastify = require("fastify")({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  return { test: "Hello World Luiz" };
+});
+
+const bootstrap = async () => {
+  try {
+    await fastify.listen({ port: 8090 });
+    fastify.log.info(`Servidor rodando em: ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+bootstrap();
